a85a9fd1b94265fb2af5b4f13225d5437640dcb2
Arman Imani <armanimani87@gmail.com>
2020-06-30 23:32:59 +0200

    Added the context of the main engine to the application

---
+++ Engine/Application/Context.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Engine/Common/common.hpp"
+#include "Engine/Entity/entity.hpp"
+
+namespace nova
+{
+	struct NOVA_API Context
+	{
+		Context()
+		{
+			master_entity = entity_manager.create();
+		}
+		
+		EntityManager entity_manager{};
+		Entity master_entity{};
+	};
+}
