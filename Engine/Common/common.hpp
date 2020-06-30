bee2453aa08b8759d5718dd106e342b684625909
Arman Imani <armanimani87@gmail.com>
2020-06-30 23:26:59 +0200

    Added a common class that defines the signatures of the system

---
+++ Engine/Common/common.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef NOVA_BUILD_DLL
+	#define NOVA_API __declspec(dllexport)
+#else
+	#define NOVA_API __declspec(dllimport)
+#endif
+
+// Disable warning for STL using in private data member
+#pragma warning(disable:4251)
+
+#include "Engine/Common/constants.hpp"
+#include "Engine/Types/base_types.hpp"
+
+namespace nova
+{
+	constexpr const char* k_engine_channel = "Engine";
+}
