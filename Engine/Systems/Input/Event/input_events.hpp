519a6d0d7fd555b5a93197235924959d618cbbbf
Arman Imani <armanimani87@gmail.com>
2020-07-06 22:21:54 +0200

    Added the input events to a generic header

--- Engine/Systems/Input/Event/input_events.hpp
+++ Engine/Systems/Input/Event/input_events.hpp
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "Engine/Systems/Input/Event/KeyDownEvent.hpp"
+#include "Engine/Systems/Input/Event/KeyUpEvent.hpp"
+#include "Engine/Systems/Input/Event/MouseDownEvent.hpp"
+#include "Engine/Systems/Input/Event/MouseUpEvent.hpp"
+#include "Engine/Systems/Input/Event/MouseWheelEvent.hpp"
+#include "Engine/Systems/Input/Event/MouseMoveEvent.hpp"
