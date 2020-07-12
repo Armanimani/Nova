c0c259b5e68b9fb271c42d7c1a8c1ec221239d88
Arman Imani <armanimani87@gmail.com>
2020-07-12 22:21:48 +0200

    Added a new abstraction for the descriptor heap

---
+++ Engine/Systems/Graphics/Platform/DirectX12/DX12DescriptorHeap.cpp
@@ -0,0 +1,58 @@
+#include "Engine/Systems/Graphics/Platform/DirectX12/DX12DescriptorHeap.hpp"
+#include "Engine/Logging/ConsoleLogger.hpp"
+
+namespace nova
+{
+	DX12DescriptorHeap::DX12DescriptorHeap(ID3D12Device* device, 
+		const D3D12_DESCRIPTOR_HEAP_TYPE type, const UInt32 number, const D3D12_DESCRIPTOR_HEAP_FLAGS flags)
+		: number{ number }, type{ type }
+	{
+		const auto description = getDescription(type, size, flags);
+		if (FAILED(device->CreateDescriptorHeap(&description, IID_PPV_ARGS(&heap))))
+			ConsoleLogger::logCritical(k_dx12_channel, "Unable to create descriptor heap!");
+
+		size = device->GetDescriptorHandleIncrementSize(type);
+		cpu_handle = heap->GetCPUDescriptorHandleForHeapStart();
+		gpu_handle = heap->GetGPUDescriptorHandleForHeapStart();
+	}
+
+	UInt32 DX12DescriptorHeap::getNumber() const noexcept
+	{
+		return number;
+	}
+	UInt32 DX12DescriptorHeap::getSize() const noexcept
+	{
+		return size;
+	}
+	
+	D3D12_DESCRIPTOR_HEAP_TYPE DX12DescriptorHeap::getType() const noexcept
+	{
+		return type;
+	}
+	
+	D3D12_CPU_DESCRIPTOR_HANDLE DX12DescriptorHeap::getCPUHandle() const noexcept
+	{
+		return cpu_handle;
+	}
+	
+	D3D12_GPU_DESCRIPTOR_HANDLE DX12DescriptorHeap::getGPUHandle() const noexcept
+	{
+		return gpu_handle;
+	}
+
+	ID3D12DescriptorHeap* DX12DescriptorHeap::getNative() const noexcept
+	{
+		return heap.Get();
+	}
+
+	D3D12_DESCRIPTOR_HEAP_DESC DX12DescriptorHeap::getDescription(
+		const D3D12_DESCRIPTOR_HEAP_TYPE type, const UInt32 number, const D3D12_DESCRIPTOR_HEAP_FLAGS flags) noexcept
+	{
+		D3D12_DESCRIPTOR_HEAP_DESC description{};
+		description.Type = type;
+		description.NumDescriptors = number;
+		description.Flags = flags;
+
+		return description;
+	}
+}
---
+++ Engine/Systems/Graphics/Platform/DirectX12/DX12DescriptorHeap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Engine/Systems/Graphics/Platform/DirectX12/DX12Common.hpp"
+#include "Engine/Common/common.hpp"
+
+namespace nova
+{
+	class NOVA_API DX12DescriptorHeap
+	{
+	public:
+		DX12DescriptorHeap(ID3D12Device* device, D3D12_DESCRIPTOR_HEAP_TYPE type, UInt32 number, D3D12_DESCRIPTOR_HEAP_FLAGS flags);
+
+		[[nodiscard]] UInt32 getNumber() const noexcept;
+		[[nodiscard]] UInt32 getSize() const noexcept;
+		[[nodiscard]] D3D12_DESCRIPTOR_HEAP_TYPE getType() const noexcept;
+		[[nodiscard]] D3D12_CPU_DESCRIPTOR_HANDLE getCPUHandle() const noexcept;
+		[[nodiscard]] D3D12_GPU_DESCRIPTOR_HANDLE getGPUHandle() const noexcept;
+		
+		[[nodiscard]] ID3D12DescriptorHeap* getNative() const noexcept;
+	private:
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> heap{};
+		UInt32 number{};
+		UInt32 size{};
+		D3D12_DESCRIPTOR_HEAP_TYPE type{};
+		D3D12_CPU_DESCRIPTOR_HANDLE cpu_handle{};
+		D3D12_GPU_DESCRIPTOR_HANDLE gpu_handle{};
+
+		[[nodiscard]] static D3D12_DESCRIPTOR_HEAP_DESC getDescription(
+			D3D12_DESCRIPTOR_HEAP_TYPE type, UInt32 number, D3D12_DESCRIPTOR_HEAP_FLAGS flags) noexcept;
+	};
+}
+
